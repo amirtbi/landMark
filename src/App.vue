af17cacf94661ef1729bdf09237706396fb10718
amirhosein <torabi46@gmail.com>
2022-02-08 13:37:27 -0500

    updating some styles

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  padding: 0;
+  margin: 0;
+  direction: rtl;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="bg-hero bg-cover w-fll">
+    <div
+      class="container py-[80px] md:py-[120px] flex flex-col items-center text-center"
+    >
+      <img
+        class="w-[125px] mb-14 lg:w-[140px]"
+        src="../assets/images/logo.svg"
+        alt="clipboard logo"
+      />
+      <h1 class="text-3xl md:text-4xl mb-4 text-dark-gray-blue">
+        A history of everything you copy
+      </h1>
+      <p
+        class="max-w-[500px] md:max-w-[700px] first-line:font-bold selection:bg-strong-cyan selection:text-white text-gray-blue md:text-[18px] mb-16 text-[14px]"
+      >
+        Clipboard allows you to track and organize everything you copy.
+        Instantly access your clipboard on all your devices.
+      </p>
+      <div
+        class="flex flex-col sm:flex-row gap-5 items-center justify-center mt-2"
+      >
+        <button class="btn btn-strong-cyan">Download for iOS</button>
+        <button class="btn btn-light-blue">Download for Mac</button>
+      </div>
+    </div>
+  </header>
+</template>
