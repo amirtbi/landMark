25578ea1308c85d0c5d24d42ec9cb158460a4f45
amirhosein <torabi46@gmail.com>
2022-02-08 13:37:46 -0500

    add some initial files

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  content: [
+
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'strong-cyan': 'hsl(171, 66%, 44%)',
+        'light-blue': 'hsl(233, 100%, 69%)',
+        'dark-gray-blue': 'hsl(210, 10%, 33%)',
+        'gray-blue': 'hsl(201, 11%, 66%)'
+      },
+      backgroundImage: {
+        hero:"url('./src/assets/images/bg-header-desktop.png')"
+      }
+    },
+    fontFamily: {
+      baiJam: 'Bai Jamjuree , sans-serif'
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: '1440px',
+        xl: '1440px',
+        '2xl':'1440px'
+      }
+    }
+    
+  },
+  plugins: [],
+}
