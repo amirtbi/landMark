c8e1901ba52327647d7b0397776bc0529fd91a9a
amir <torabi46@gmail.com>
2022-02-12 19:08:08 +0330

    create logo list component

---
+++ src/components/CompanyLogos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="logo" alt="company" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["logoIcon"],
+  computed: {
+    logo() {
+      // Binding dynamic image source in 'Vite'
+      // In case of vueCli, use require
+      const path = `../assets/images/${this.logoIcon}.png`;
+      const modules = import.meta.globEager("../assets/images/*.png");
+      return modules[path].default;
+    },
+  },
+};
+</script>
